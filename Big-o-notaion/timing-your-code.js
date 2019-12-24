d679290da17dc52b4ef314942c6efb6318de314f
chihab <adamchihab2012@hotmail.com>
2019-12-24 10:37:34 +0000

    timing your code intro

--- Big-o-notaion/timing-your-code.js
+++ Big-o-notaion/timing-your-code.js
@@ -12,16 +12,29 @@ for (let i = 0; i <= n; i++) {
      return total
  }
 
-//  timing of the first solution
-//  console.log( sumSolution1(5))
-let t1 = performance.now();
-sumSolution1(1000000000);
-let t2 = performance.now();
-console.log(`sum solution 1 ${(t2- t1)} / 1000} seconds.` )
-
 //  Solution 2
 const sumSolution2 =(n) => { 
     return n * (n + 1 ) / 2;
 }
 
-// console.log(sumSolution2(5))
+//  Using timing 
+/**
+ * different machines will record different times
+ * the same machine will record different times!
+ * for faster algoritms, speed measurements many not be precise 
+ * 
+ * For example using
+ * let t1 = preformance.now()
+ */
+
+//  Using operation computation
+/**
+ *  Time is determined by the operation type
+ * for example: 
+ * in solution2, we have 1 mult, 1 add and 1 div 
+ * so there are 3 calculation only
+ * 
+ * In solution 1:
+ * we have more operation, so if N is 20 there are 20 operation and more 
+ * As N grows , the number of operation grows with N
+ */
